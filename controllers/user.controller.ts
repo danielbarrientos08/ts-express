e9db36fe8f966b355f7eb048fd22155f81c7880f
DanielBarrientos <danielbarrientos0802@gmail.com>
2022-05-03 15:09:48 -0500

    crud usuarios

--- controllers/user.controller.ts
+++ controllers/user.controller.ts
@@ -1,43 +1,102 @@
 import { Request, Response } from 'express';
+import User from '../models/user.model';
 
-export const getUsers = (req: Request, res: Response) => {
+export const getUsers = async (req: Request, res: Response) => {
+
+  const users = await User.findAll();
+
   res.json({
-    msg: 'getUsers',
+    users
   });
 };
 
-export const getUser = (req: Request, res: Response) => {
+export const getUser = async(req: Request, res: Response) => {
   const { id } = req.params;
+  const user = await User.findByPk(id)
 
+  if(!user) {
+    res.status(404).json({
+      msg:'User not found'
+    });
+  }
   res.json({
-    msg: 'getUser',
+   user
   });
 };
 
-export const postUser = (req: Request, res: Response) => {
+export const postUser = async(req: Request, res: Response) => {
   const { body } = req;
+  
+  try {
+    const existsEmail = await User.findOne({
+      where: {
+        email:body.email
+      }
+    });
 
-  res.json({
-    msg: 'postUser',
-    body,
-  });
+    if (existsEmail) {
+      return res.status(400).json({
+        msg: `the ${body.email} is already in use`
+      });
+    }
+
+    const user = new User(body);
+    await user.save();
+    res.json({
+      user
+    });
+  } catch (error) {
+    res.status(500).json({
+      msg: 'unhandled error (500)'
+    });
+  }
+  
 };
 
-export const putUser = (req: Request, res: Response) => {
+export const putUser = async(req: Request, res: Response) => {
   const { body } = req;
   const { id } = req.params;
 
-  res.json({
-    msg: 'putUser',
-    body,
-    id,
-  });
+  try {
+    const user = await User.findByPk(id);
+    if(!user) {
+      return res.status(404).json({
+        msg:'User not found'
+      });
+    }
+        
+    await user.update(body);
+
+    return res.json({
+      user
+    });
+  } catch (error) {
+    res.status(500).json({
+      msg: 'unhandled error (500)'
+    });
+  }
 };
-export const deleteUser = (req: Request, res: Response) => {
+export const deleteUser = async(req: Request, res: Response) => {
   const { id } = req.params;
 
-  res.json({
-    msg: 'deleteUser',
-    id,
-  });
+  try {
+    const user = await User.findByPk(id);
+    if(!user) {
+      return res.status(404).json({
+        msg:'User not found'
+      });
+    }
+
+    await user.update({status:false});
+    // await user.destroy();
+    res.json({
+      msg: 'user deleted',
+     
+    });
+  } catch (error) {
+    res.status(500).json({
+      msg: 'unhandled error (500)'
+    });
+  }
+ 
 };
